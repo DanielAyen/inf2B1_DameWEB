0cd6309a149c248296ddd2cff93f5a1145498bc7
DanielAyen <Daniel.Ayen@student.reutlingen-university.de>
2016-01-04 14:57:36 +0100

    Zugservlet etwas logik spiel jsp mit brett ....

--- DameWeb/WebContent/SpielJSP.jsp
+++ DameWeb/WebContent/SpielJSP.jsp
@@ -7,18 +7,49 @@
 <title>Spiel</title>
 </head>
 <body>
+
+	<%
+		System.out.println("JAVA Console Blabla");
+		java.util.Date date = new java.util.Date();
+		int cnt = 0;
+		String ausgabe = "";
+		ausgabe = "<table border=\"1\"> <tr>";
+		boolean ss = false;
+
+		for (int i = 11; i >= 0; i--) {
+			for (int j = 0; j < 12; j++) {
+				
+				if (ss == false) {
+					ausgabe += "<td><a href=zugServlet?datum=a4><img src=\"feldw.png\" alt=\"W X:" + i + "Y:" + j + "\"></a></td>";
+					ss = !ss;
+					cnt++;
+				} else {
+					ausgabe += "<td><a href=zugServlet><img src=\"felds.png\" alt=\"S X:" + i + "Y:" + j + "\"></a></td>";
+					//\"https://www.google.de\"
+					ss = !ss;
+					cnt++;
+				}
+				
+				if (cnt == 12) {
+					ss = !ss;
+					ausgabe+="</tr><tr>";
+					cnt = 0;
+				}
+			}
+		}
+		
 	
-<% System.out.println( "JAVA Console Blabla" ); 
-java.util.Date date = new java.util.Date(); 
+		
 
-out.println("HTML TEST" );       
-out.println("<br/>");
-out.println("date : " + date + "Blabla");
+		out.println("HTML TEST");
+		out.println("<br/>");
+		out.println("date : " + date + "Blabla");
+		ausgabe+="</tr>";
+		out.println(ausgabe);
+	%>
 
-%> 
+	<h1>TEST</h1>
+	<p>HUHU</p>
 
-<h1>TEST</h1>
-<p> HUHU </p>
-
-	</body>
+</body>
 </html>
---
+++ DameWeb/src/WebGUI/zugServlet.java
@@ -0,0 +1,47 @@
+package WebGUI;
+
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class zugServlet
+ */
+@WebServlet("/zugServlet")
+public class zugServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public zugServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String datum=request.getParameter("datum");
+		System.out.println(request.getParameter("datum"));
+		if (Pattern.matches("[a-l A-L][0-11]",datum)){
+			System.out.println("mhm");
+		}System.out.println("ne");
+		response.sendRedirect("SpielJSP.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	}
+}
